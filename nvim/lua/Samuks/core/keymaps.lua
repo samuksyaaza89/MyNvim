5d1a85376c463d6da39ae8f4e11d3f9d5301393b
samuksyaaza <samuksyaaza@gmail.com>
2025-10-29 20:23:46 -0300

    Atualizar o keymaps.lua

--- nvim/lua/Samuks/core/keymaps.lua
+++ nvim/lua/Samuks/core/keymaps.lua
@@ -21,3 +21,39 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- === Função para rodar arquivos Python ===
+function _G.RunPythonFile()
+  local current_file = vim.fn.expand("%:p")
+  if current_file:match("%.py$") then
+    vim.cmd("!python3 %")
+  else
+    print("Not a Python file.")
+  end
+end
+vim.api.nvim_set_keymap("n", "<leader>rp", ":lua _G.RunPythonFile()<CR>", { noremap = true, silent = true })
+
+-- === Função para compilar e rodar arquivos C ===
+function _G.RunCFile()
+  local current_file = vim.fn.expand("%:p")
+  if current_file:match("%.c$") then
+    local output_file = vim.fn.expand("%:r") -- mesmo nome, sem extensão
+    vim.cmd(string.format("!gcc -o %s %s && ./%s", output_file, current_file, output_file))
+  else
+    print("Not a C file.")
+  end
+end
+vim.api.nvim_set_keymap("n", "<leader>rc", ":lua _G.RunCFile()<CR>", { noremap = true, silent = true })
+
+-- === Função para compilar e rodar arquivos C++ ===
+function _G.RunCppFile()
+  local current_file = vim.fn.expand("%:p")
+  if current_file:match("%.cpp$") then
+    local output_file = vim.fn.expand("%:r")
+    vim.cmd(string.format("!g++ -o %s %s && ./%s", output_file, current_file, output_file))
+  else
+    print("Not a C++ file.")
+  end
+end
+vim.api.nvim_set_keymap("n", "<leader>rpp", ":lua _G.RunCppFile()<CR>", { noremap = true, silent = true })
+
